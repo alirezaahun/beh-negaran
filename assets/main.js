2de98334636ad9b0ebf2048686b57c4209ce7b30
sohrab <sohrabnasehi.sn@gmail.com>
2022-04-16 16:04:43 +0430

    added numbers trigger on scroll

--- assets/main.js
+++ assets/main.js
@@ -1,123 +1,125 @@
 // Persian Date ------------------------------------------------
 $(document).ready(function () {
   const datepickerDOM = $("#persianDatapicker");
-  const dateObject = datepickerDOM.persianDatepicker(
-      {
-      "inline": false,
-      "format": "LLLL",
-      "viewMode": "day",
-      "initialValue": true,
-      "minDate": false,
-      "maxDate": false,
-      "autoClose": false,
-      "position": "auto",
-      "altFormat": "lll",
-      "altField": "#altfieldExample",
-      "onlyTimePicker": false,
-      "onlySelectOnDate": false,
-      "calendarType": "persian",
-      "inputDelay": 800,
-      "observer": false,
-      "calendar": {
-          "persian": {
-              "locale": "fa",
-              "showHint": true,
-              "leapYearMode": "algorithmic"
-          },
-          "gregorian": {
-              "locale": "en",
-              "showHint": true
-          }
+  const dateObject = datepickerDOM.persianDatepicker({
+    inline: false,
+    format: "LLLL",
+    viewMode: "day",
+    initialValue: true,
+    minDate: false,
+    maxDate: false,
+    autoClose: false,
+    position: "auto",
+    altFormat: "lll",
+    altField: "#altfieldExample",
+    onlyTimePicker: false,
+    onlySelectOnDate: false,
+    calendarType: "persian",
+    inputDelay: 800,
+    observer: false,
+    calendar: {
+      persian: {
+        locale: "fa",
+        showHint: true,
+        leapYearMode: "algorithmic",
       },
-      "navigator": {
-          "enabled": true,
-          "scroll": {
-              "enabled": true
-          },
-          "text": {
-              "btnNextText": "<",
-              "btnPrevText": ">"
-          }
+      gregorian: {
+        locale: "en",
+        showHint: true,
       },
-      "toolbox": {
-          "enabled": true,
-          "calendarSwitch": {
-              "enabled": true,
-              "format": "MMMM"
-          },
-          "todayButton": {
-              "enabled": true,
-              "text": {
-                  "fa": "امروز",
-                  "en": "Today"
-              }
-          },
-          "submitButton": {
-              "enabled": true,
-              "text": {
-                  "fa": "تایید",
-                  "en": "Submit"
-              }
-          },
-          "text": {
-              "btnToday": "امروز"
-          }
+    },
+    navigator: {
+      enabled: true,
+      scroll: {
+        enabled: true,
       },
-      "timePicker": {
-          "enabled": false,
-          "step": 1,
-          "hour": {
-              "enabled": true,
-              "step": null
-          },
-          "minute": {
-              "enabled": true,
-              "step": null
-          },
-          "second": {
-              "enabled": true,
-              "step": null
-          },
-          "meridian": {
-              "enabled": false
-          }
+      text: {
+        btnNextText: "<",
+        btnPrevText: ">",
       },
-      "dayPicker": {
-          "enabled": true,
-          "titleFormat": "YYYY MMMM"
+    },
+    toolbox: {
+      enabled: true,
+      calendarSwitch: {
+        enabled: true,
+        format: "MMMM",
       },
-      "monthPicker": {
-          "enabled": true,
-          "titleFormat": "YYYY"
+      todayButton: {
+        enabled: true,
+        text: {
+          fa: "امروز",
+          en: "Today",
+        },
       },
-      "yearPicker": {
-          "enabled": true,
-          "titleFormat": "YYYY"
+      submitButton: {
+        enabled: true,
+        text: {
+          fa: "تایید",
+          en: "Submit",
+        },
       },
-      "responsive": true,
-      "onSelect" : function(){
-          
-          alert(`تاریخ انتخاب شده : ${date.year}/${date.month}/${date.date} ~ ${date.hour}:${date.minute}:${date.second}`);
-      }
+      text: {
+        btnToday: "امروز",
+      },
+    },
+    timePicker: {
+      enabled: false,
+      step: 1,
+      hour: {
+        enabled: true,
+        step: null,
+      },
+      minute: {
+        enabled: true,
+        step: null,
+      },
+      second: {
+        enabled: true,
+        step: null,
+      },
+      meridian: {
+        enabled: false,
+      },
+    },
+    dayPicker: {
+      enabled: true,
+      titleFormat: "YYYY MMMM",
+    },
+    monthPicker: {
+      enabled: true,
+      titleFormat: "YYYY",
+    },
+    yearPicker: {
+      enabled: true,
+      titleFormat: "YYYY",
+    },
+    responsive: true,
+    onSelect: function () {
+      alert(
+        `تاریخ انتخاب شده : ${date.year}/${date.month}/${date.date} ~ ${date.hour}:${date.minute}:${date.second}`
+      );
+    },
   });
 
   const date = dateObject.getState().view;
-   
 });
 
+// Trigger Function On Scroll ----------------------------------------
 
-// ScrollBar Top ----------------------------------------
-// const scrollProgress = document.getElementById("scroll-progress");
-// const height =
-//   document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  $(allInView);
+  $(window).scroll(allInView);
+  
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+  
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+  
+    return elemBottom <= docViewBottom && elemTop >= docViewTop;
+  }
 
-// window.addEventListener("scroll", () => {
-//   const scrollTop =
-//     document.body.scrollTop || document.documentElement.scrollTop;
-//   scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
-// });
-
-// Numbers -------------------------------------
+// Numbers Count Update -------------------------------------
 const items = [...document.querySelectorAll(".number")];
 
 const updateCount = (el) => {
@@ -139,23 +141,28 @@ const updateCount = (el) => {
   }, 1);
   // console.log(increaseCount);
 };
+// Trigger Numbers Function -----------------------------------------------
+function allInView() {
+  if (isScrolledIntoView($("#numbers"))) {
+    items.forEach((item) => {
+      updateCount(item);
+    });
+  }
 
-items.forEach((item) => {
-    updateCount(item);
-  });
+}
 
-
-  var coll = document.getElementsByClassName("collapsible-trigger");
+// Footer Items Collapse On Mobile ------------------------------
+var coll = document.getElementsByClassName("collapsible-trigger");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("collapse-active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+    }
   });
 }
