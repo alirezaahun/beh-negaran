2ed17415f5c752881c8e375ccc74b51c8ca85f5d
sohrab <sohrabnasehi.sn@gmail.com>
2022-04-18 19:53:17 +0430

    fix

--- assets/main.js
+++ assets/main.js
@@ -173,7 +173,7 @@ for (i = 0; i < coll.length; i++) {
 // Change Service Box Display On Scroll ---------------------------------------
 $(document).ready(function () {
   $(window).scroll(function () {
-    if ($(window).scrollTop() > 600) {
+    if ($(window).scrollTop() > 850) {
       $(".quick-service-box").css({ visibility: "hidden" });
       $(".service-icons-mobile").css({ display: "block" });
       $(".service-icons-mobile").css({ backgroundColor: "#f6f6f6" });
@@ -183,3 +183,4 @@ $(document).ready(function () {
     }
   });
 });
+
