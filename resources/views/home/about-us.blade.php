2087f3e209cb39adfb3e894e33d296554fdf6c02
Alireza <alirezaabadi2016@gmail.com>
2022-04-22 20:48:54 +0430

    edit about us page

--- resources/views/home/about-us.blade.php
+++ resources/views/home/about-us.blade.php
@@ -8,7 +8,395 @@
     <div class="banner">
     </div>
 
+
+    <div class="page-content container my-5">
+
+    <div class="row justify-content-right">
+      <div class="col col-md-8 text-right">
+        <h3>
+          چرا هستیم؟
+        </h3>
+        <hr class="w-25">
+
+        <p >
+          زندگی این روزها به قدری سریع جریان داره که یک روز چشمامونو باز می کنیم می بینیم سالها گذشته و از خاطرات ریز و درشت مون هاله ای بیش نمونده! ما برای این اینجا هستیم که خاطرات شما،
+          مهمترین لحظات و شادی هاتون رو به راحت ترین روش ثبت و ضبط کنیم تا هر وقت اراده کردید،
+          راحت برگردید به گذشته و خاطره ها رو مرور کنید.
+        </p>
+        <p class="lead text-right">
+          کادرو اینجاست تا شما بتوانید با یک نگاه،
+          <span style="color: rgb(61, 142, 185);"> خاطرات زندگی تان را لمس کنید.</span>
+        </p>
+      </div>
     </div>
+
+
+    <div class="row text-center justify-content-center my-5" >
+      <div class="col-8">
+        <h3>اعضای تیم کادرو</h3>
+        <p class="lead text-center">
+          ما بهترین های ایران رو دور خودمون جمع کرده ایم، این شامل
+          ۹۹۱۸
+          عکاسی که تیم ما رو تشکیل داده اند هم میشه.
+        </p>
+      </div>
+    </div>
+
+
+    <div class="row justify-content-statr align-c text-left my-5">
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="امیرمهدی واله هم بنیانگذار و مدیرعامل " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/1/large_avatar_1.webp" id="https://app.kadro.co/uploads/admin_user/avatar/1/large_avatar_1.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+               
+                    امیرمهدی واله
+                </h3>
+                <p class="lead">
+                  هم بنیانگذار و مدیرعامل
+                </p>
+                <p>
+                  امیر مهدی پیش از این تو یه شرکت نرم افزاری حوزه‌ی سلامت الکترونیک فعالیت کرده. ایشون یه برنامه نویس full stack هست که به سادگی ایده رو به واقعیت تبدیل می‌کنه. امیر مهدی در سال ۹۴، منوچی رو تاسیس کرد، معلم زبانه و به دنبال موفقیت‌های بزرگ قدم برمی‌داره.
+                </p>
+              </div>
+              <p class="lead">
+                <a href="https://twitter.com/amvaleh" class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/i.amvaleh/" class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/amvaleh/" class="mx-2"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="محمد مهدی حسینی نسب هم بنیان گذار و مدیر مارکتینگ " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/4/large_avatar_4.webp" id="https://app.kadro.co/uploads/admin_user/avatar/4/large_avatar_4.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+                 
+                    محمد مهدی حسینی نسب
+                </h3>
+                <p class="lead">
+                  هم بنیان گذار و مدیر مارکتینگ
+                </p>
+                <p>
+                  علاقه‌ی مهدی به تدریس و عکاسی باعث شد تا یه مدت تو مدرسه و بعد از اون در مجموعه‌ی تبیان فعالیت کنه و بعد با دریافت مدرک عکاسی و فعالیت تو آتلیه‌ی شخصی، راه رو برای کار تو بقیه‌ی حرفه‌های مورد علاقه‌ش هموارتر کرد. از دانش مهدی در بازاریابی دیجیتال و تولید محتوا استفاده می‌کنیم.
+                </p>
+              </div>
+              <p class="lead">
+                <a href="https://twitter.com/mmahdihn" class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/mmahdihn" class="mx-2"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="سیاوش باقری هم بنیانگذار و مدیر فروش " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/26/large_avatar_26.webp" id="https://app.kadro.co/uploads/admin_user/avatar/26/large_avatar_26.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+                 
+                    سیاوش باقری
+                </h3>
+                <p class="lead">
+                  هم بنیانگذار و مدیر فروش
+                </p>
+                <p>
+                  سیاوش دانش آموخته کانون سینماگران جوان است. حوزه تخصصی او عکاسی معماری و صنعتی با بیش از ۱۰ سال تجربه است. اشتیاق به یادگیری و حرفه ای بودن ۲ ویژگی بارز اوست؛ سیاوش عاشق پروژه های عکاسی چالش برانگیزه و به شدت به نقد اثرگذار اعتقاد داره.
+                </p>
+              </div>
+              <p class="lead">
+                <a href="https://twitter.com/Siavashbagheri" class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://www.instagram.com/siaavaashh" class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/siavashbagheri" class="mx-2"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="رضا کمال خانی مدیر فنی و برنامه‌نویس " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/43/large_avatar_43.webp" id="https://app.kadro.co/uploads/admin_user/avatar/43/large_avatar_43.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+              
+                    رضا کمال خانی
+                </h3>
+                <p class="lead">
+                  مدیر فنی و برنامه‌نویس
+                </p>
+                <p>
+                  رضا فارغ التحصیل رشته تجارت الکترونیک است. در طراحی و پیاده سازی وب، علوم داده، مدیریت کسب و کار های دیجیتال بیش از ۳ ساله فعالیت داره و علاقه زیادی به تکنولوژی های جدید و به روز داره. هسته مرکزی الگوریتم پشنهادی کادرو هم توسط رضا پیاده سازی شده است.
+                </p>
+              </div>
+              <p class="lead">
+                <a href="https://www.linkedin.com/in/reza-khani/" class="mx-2"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="حسین حجازی مدیر دپارتمان کادرو کلیپ " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/40/large_avatar_40.webp" id="https://app.kadro.co/uploads/admin_user/avatar/40/large_avatar_40.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+                
+                    حسین حجازی
+                </h3>
+                <p class="lead">
+                  مدیر دپارتمان کادرو کلیپ
+                </p>
+                <p>
+                  حسین فارغ التحصیل رشته‌ی معماری است و علاقه‌اش به کادر بندی رو از چهارم دبستان کشف کرد. ایده پردازی های او و علاقه‌ی اش به کارآفرینی باعث پیشرفت روزافزون کادرو خواهد شد.
+                </p>
+              </div>
+              <p class="lead">
+                <a href="https://www.instagram.com/hos3ln/" class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="مریم اسفندیاری مسئول خدمات جانبی و کنترل کیفیت " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/35/large_avatar_35.webp" id="https://app.kadro.co/uploads/admin_user/avatar/35/large_avatar_35.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+               
+                    مریم اسفندیاری
+                </h3>
+                <p class="lead">
+                  مسئول خدمات جانبی و کنترل کیفیت
+                </p>
+                <p>
+                  مریم فارغ التحصیل رشته بازرگانی هستش و از اونجاییکه اعتقاد داره با ویرایش عکس ها میشه به خروجی جذاب تری دست پیدا کرد، مسئول کنترل کیفیت و خدمات جانبی عکاسی شد.
+                </p>
+              </div>
+              <p class="lead">
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="اکرم اسفندیاری عکاس و مسئول کنترل کیفیت " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/46/large_avatar_46.webp" id="https://app.kadro.co/uploads/admin_user/avatar/46/large_avatar_46.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+                  
+                    اکرم اسفندیاری
+                </h3>
+                <p class="lead">
+                  عکاس و مسئول کنترل کیفیت
+                </p>
+                <p>
+                  اکرم به شدت دقیق و نکته سنج هستش، از کنار کوچک ترین مسائل هم به راحتی عبور نمی کنه، به خاطر همین ویژگی متمایزش تو بخش کنترل کیفیت به کمک سیاوش مو رو از ماست میکشن بیرون. خیالتون راحت که اکرم حواسش به همه چیز هست.
+                </p>
+              </div>
+              <p class="lead">
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="نازنین کریمی مشاور فروش و پشتیبانی " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/57/large_avatar_57.webp" id="https://app.kadro.co/uploads/admin_user/avatar/57/large_avatar_57.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+                  
+                    نازنین کریمی
+                </h3>
+                <p class="lead">
+                  مشاور فروش و پشتیبانی
+                </p>
+                <p>
+                  قطعا، کلمات در توصیف اشتیاق، انکیزه و مهارت‌های نازنین ناتوانند. نازنین که کارشناسی ارشد فیزیک داره، از دوران نوجوانی، علاقه فراوانی به عکاسی داشته و همیشه در این مسیر پیشرو بوده. مسئولیت‌پذیری، پشتکار و تلاش برای رشد در همه زمینه‌های شغلی و زندگی از ویژگی‌های بازر نازنینه که به خوبی در کادرو هم نمایان است.
+                </p>
+              </div>
+              <p class="lead">
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="نگار ناطقی پور مشاور جذب عکاس " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/47/large_avatar_47.webp" id="https://app.kadro.co/uploads/admin_user/avatar/47/large_avatar_47.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+                
+                    نگار ناطقی پور
+                </h3>
+                <p class="lead">
+                  مشاور جذب عکاس
+                </p>
+                <p>
+                  نگار فارغ التحصیل رشته ی مدیریت صنعتی است . علاقه ی او به هنر و عکاسی باعث شد در این راه قدم بذاره و دوره های عکاسی مقدماتی و پیشرفته رو بگذراونه. به خاطر روابط عمومی خوبی هم که داره، کلی عکاس خفن و حرفه‌ای برای کادرو تونسته جذب کنه.
+                </p>
+              </div>
+              <p class="lead">
+                <a href="https://twitter.com/Thenegar_" class="mx-2"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://instagram.com/_negarn_" class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/negar-nateghi-171029200" class="mx-2"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-lg-6">
+        <div class="row align-items-start">
+          <div class="col-4">
+            <div class="fr-wrapper" dir="auto">
+              <div aria-disabled="false" contenteditable="true" dir="auto" spellcheck="true">
+                <p>
+                  <img alt="عرفان اکبری طراح و گرافیست " class="img-fluid rounded fr-fic fr-dii img-replace" src="https://app.kadro.co/uploads/admin_user/avatar/66/large_avatar_66.webp" id="https://app.kadro.co/uploads/admin_user/avatar/66/large_avatar_66.webp">
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-8 ">
+            <div>
+              <div>
+                <h3 class="text-right">
+                
+                    عرفان اکبری
+                </h3>
+                <p class="lead">
+                  طراح و گرافیست
+                </p>
+                <p>
+                  عرفان، یک خوره‌ی تکنولوژی!‌ وی که دانشجوی رشته مهندسی نرم افزار است، طراحی و تدوین روح کادرو در شبکه‌های اجتماعی رو بر عهده داره.
+                </p>
+              </div>
+              <p class="lead">
+                <a href="https://instagram.com/akerfan1" class="mx-2"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.linkedin.com/in/erfan-akbari-127a55171" class="mx-2"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+    <div class="row align-items-center">
+      <div class="col-12  col-md-6 col-lg-5">
+        <h2 class="text-center">
+          کنارتان هستیم!
+        </h2>
+        <p class="text-center"> ما در شبکه های اجتماعی هم کنارتان هستیم! </p>
+        <p class="text-center">
+          <a href="https://twitter.com/kadro_co" class="mx-2 text-primary"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="https://www.facebook.com/kadro.co" class="mx-2 text-primary"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="https://www.instagram.com/kadro.official/" class="mx-2 text-primary"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="https://www.pinterest.ch/kadro_co/" class="mx-2 text-primary"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+          <a href="https://api.whatsapp.com/send?phone=0989027048963" class="mx-2 text-primary"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+        </p>
+      </div>
+      <div class="col-12 col-md-6 ml-md-auto mt-4 mt-md-0">
+        <img alt="image" class="img-fluid" src="https://app.kadro.co/img/aboutus.jpg">
+      </div>
+    </div>
+    </div>
+
+
+  </div>
 </main>
 
 @endsection
