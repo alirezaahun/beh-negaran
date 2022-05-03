915341bf2790cdf1783026eeb3e6d60e85d099d3
sohrab <sohrabnasehi.sn@gmail.com>
2022-05-03 19:45:41 +0430

    added login form

--- public/js/main.js
+++ public/js/main.js
@@ -3295,9 +3295,42 @@ window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}; // Active Mobile Bottom Menu Link ------------------------------------
-// Get the container element
+}; // Form----------------
 
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  var $this = $(this),
+      label = $this.prev('label');
+
+  if (e.type === 'keyup') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    } else {
+      label.addClass('active highlight');
+    }
+  } else if (e.type === 'blur') {
+    if ($this.val() === '') {
+      label.removeClass('active highlight');
+    } else {
+      label.removeClass('highlight');
+    }
+  } else if (e.type === 'focus') {
+    if ($this.val() === '') {
+      label.removeClass('highlight');
+    } else if ($this.val() !== '') {
+      label.addClass('highlight');
+    }
+  }
+});
+$('.tab a').on('click', function (e) {
+  e.preventDefault();
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  target = $(this).attr('href');
+  $('.tab-content > div').not(target).hide();
+  $(target).fadeIn(600);
+}); // Active Mobile Bottom Menu Link ------------------------------------
+// Get the container element
 
 var navContainer = document.getElementById("navContainer"); // Get all buttons with class="btn" inside the container
 
